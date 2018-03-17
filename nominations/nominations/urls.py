40d94eb82392de47c507b6153e9b7fa5f5ffc7eb
meme-lord <meme-lord@users.noreply.github.com>
2018-03-17 16:00:55 +0000

    integrated login folder to main

--- nominations/nominations/urls.py
+++ nominations/nominations/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -7,9 +9,11 @@ urlpatterns = [
     path('', views.FormListView.as_view(), name='home'),
     path('post/<int:pk>/', views.FormDetailView.as_view(), name='post_detail'),
     #path('post/new/', views.FormCreateView.as_view(), name='post_new'),
-	path('post/submit/',views.form_submit,name='post_new')
+	path('post/submit/',views.form_submit,name='post_new'),
     #path('post/<int:pk>/edit/',
     #     views.FormUpdateView.as_view(), name='post_edit'),
     #path('post/<int:pk>/delete/',
     #     views.FormDeleteView.as_view(), name='post_delete'),
+	url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout')
 ]
