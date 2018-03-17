cf206bf6bb3ee4ce9218211fd69c1020dfc3674d
meme-lord <meme-lord@users.noreply.github.com>
2018-03-17 00:30:30 +0000

    switched form to mysql, added create table to db.sql

--- nominations/nominations/settings.py
+++ nominations/nominations/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'nominations.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'default':{
+		'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nominations',
+        'USER': 'nomination',
+        'PASSWORD': 'n0m1nationzZ10101',
+        'HOST': 'localhost',
+        'PORT': '3306'
+	}
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
 }
 
 
